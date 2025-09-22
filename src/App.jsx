52091be1e8e5e991702e3ed642b4c40b19343db2
Dinma Faith <phaytep@gmail.com>
2025-09-22 10:35:05 +0100

    chore: initialize Vite project with React and configure Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+      <div className="text-center p-10">
+        <h1 className="text-3xl font-bold text-blue-500">
+        Hello!
+
+        </h1>
+      </div>
+  )
+}
+
+export default App
