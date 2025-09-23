91c2bd944e5607fbabea67d5eeff23fbceace5b3
Dinma Faith <phaytep@gmail.com>
2025-09-23 12:50:07 +0100

    refactor(main): use reusable Input and Button components for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
-// import Main from "./pages/main";
-// import Detail from "./pages/detail";
+import Main from "./pages/main";
+import Detail from "./pages/detail";
+import ProtectedRoute from "./components/protect-route";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/main" element={<Main />} /> */}
-        {/* <Route path="/detail/:id" element={<Detail />} /> */}
+        <Route
+          path="/main"
+          element={
+            <ProtectedRoute>
+              <Main />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/detail/:id"
+          element={
+            <ProtectedRoute>
+              <Detail />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
